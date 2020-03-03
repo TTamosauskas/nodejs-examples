0384adf2bcf222938d4ae7bdc831b1ddd3e39002
TTamosauskas <thiago.gt@gmail.com>
2020-03-03 16:14:14 -0300

    exemplo de paginação

---
+++ 5-paginacao/src/routes.js
@@ -0,0 +1,12 @@
+// Importa express, routes e 
+const express = require('express');
+const routes = express.Router();
+const ProductController = require('./controllers/ProductController');
+
+routes.post('/products', ProductController.store);
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
+module.exports = routes;
